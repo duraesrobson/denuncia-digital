4595e8ca518dd316dcdb6dd077dfc7d179e92c74
Robson Durães <duraesrobson@gmail.com>
2025-04-05 23:57:50 -0300

    comeco estilizacao index

--- app/_layout.js
+++ app/_layout.js
@@ -2,8 +2,22 @@ import React from 'react';
 import { Slot, Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { COLORS } from 'app/styles/global';
+import { useFonts } from 'expo-font';
+import { LoadingScreen } from 'app/components/LoadingScreen';
 
 const IndexLayout = () => {
+  const [fontsLoaded] = useFonts({
+    'Rajdhani-Light': require('../assets/fonts/Rajdhani-Light.ttf'),
+    'Rajdhani-Regular': require('../assets/fonts/Rajdhani-Regular.ttf'),
+    'Rajdhani-Medium': require('../assets/fonts/Rajdhani-Medium.ttf'),
+    'Rajdhani-SemiBold': require('../assets/fonts/Rajdhani-SemiBold.ttf'),
+    'Rajdhani-Bold': require('../assets/fonts/Rajdhani-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <LoadingScreen />;
+  }
+
   return (
     <SafeAreaProvider>
       <Stack
@@ -14,6 +28,7 @@ const IndexLayout = () => {
           headerTintColor: COLORS.light,
           headerTitleStyle: {
             fontWeight: 'bold',
+            fontFamily: 'Rajdhani-Bold',
           },
         }}
       >
@@ -23,7 +38,7 @@ const IndexLayout = () => {
             headerShown: false
           }}
         />
-        
+    
         <Stack.Screen 
           name="(auth)/log-in" 
           options={{
