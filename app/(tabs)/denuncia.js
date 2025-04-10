860c70781f61251d756209c5a22c8a9167d70553
Robson Durães <duraesrobson@gmail.com>
2025-04-10 15:35:39 -0300

    firebase implementacao

--- app/(tabs)/denuncia.js
+++ app/(tabs)/denuncia.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert, ScrollView } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { COLORS } from 'app/styles/global';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
+import { Picker } from '@react-native-picker/picker';
 
-export default function Denuncia() {
-  return (
+export default function DenunciaScreen() {
+  const [faixaEtaria, setFaixaEtaria] = useState('');
+
+  const enviarDenuncia = async () => {
+    if (!faixaEtaria) {
+      Alert.alert('Erro', 'Preencha todos os campos');
+      return;
+    }
+
+    try {
+      await addDoc(collection(db, 'denuncias'), {
+        faixaEtaria,
+        data: new Date().toISOString()
+      });
+
+      Alert.alert('Sucesso', 'Denúncia enviada com sucesso!');
+      setFaixaEtaria('');
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Não foi possível enviar a denúncia.');
+    }
+  };
+
+return (
     <SafeAreaProvider style={styles.container}>
       <ScrollView>
-        <Text style={styles.titulo}>Denúncia</Text>
-        <Text style={styles.descricao}>Faça sua denúncia aqui</Text>
+      <View>
+      <Text style={styles.label}>Selecione sua faixa etária:</Text>
+      <Picker
+        selectedValue={faixaEtaria}
+        onValueChange={(itemValue) => setFaixaEtaria(itemValue)} 
+      >
+        <Picker.Item label='Selecione...' value='' />
+        <Picker.Item label='18-25 anos' value='18-25' />
+        <Picker.Item label="26-30 anos" value="26-30" />
+        <Picker.Item label="31-38 anos" value="31-38" />
+        <Picker.Item label="38-45 anos" value="38-45" />
+        <Picker.Item label="46-55 anos" value="46-55" />
+        <Picker.Item label="Acima de 55 anos" value="55+" />
+      
+      </Picker>
+
+      
+
+      <Button title="Enviar Denúncia" onPress={enviarDenuncia} />
+    </View>
       </ScrollView>
     </SafeAreaProvider>
   );
@@ -35,5 +78,13 @@ const styles = StyleSheet.create({
     fontWeight: 'regular',
     marginBottom: 10,
     textAlign: 'center'
+  },
+  label: { marginTop: 10 },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    borderRadius: 5,
+    marginBottom: 10
   }
 }); 
