2ec02abe05cbe8d9f02d94f931919487c6d464b2
Robson Durães <duraesrobson@gmail.com>
2025-04-14 16:08:46 -0300

    home page init content design

---
+++ app/components/ContentCard.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { COLORS } from '../styles/global';
+import OrangeButton from './OrangeButton';
+
+const ContentCard = ({ imageSource, title, description, buttonTitle, onPress }) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.imageContainer}>
+        {imageSource && (
+          <Image 
+            source={imageSource}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        )}
+      </View>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.description}>{description}</Text>
+      <OrangeButton 
+        title={buttonTitle}
+        handlePress={onPress}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: COLORS.light,
+    borderRadius: 12,
+    padding: 15,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    marginTop: 40
+  },
+  imageContainer: {
+    width: 300,
+    height: 370,
+    backgroundColor: COLORS.light,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  image: {
+    width: 385,
+    height: 318,
+    marginTop: -165,
+    marginBottom: -80
+    
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: 'Rajdhani-Bold',
+    color: COLORS.darkPurple,
+    marginBottom: 10,
+    marginTop: -40
+  },
+  description: {
+    fontSize: 18,
+    fontFamily: 'Rajdhani-Regular',
+    color: COLORS.darkPurple,
+    marginBottom: 15,
+  },
+});
+
+export default ContentCard;
